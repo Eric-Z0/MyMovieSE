9eedf44eeaa1e1e01e1aa09150e087017ee080cc
Eric-Z0 <ericzhuang603@gmail.com>
2020-01-06 16:02:43 -0500

    First Commit

---
+++ backend/MyMovieSE-Spring/src/main/java/com/luv2code/springboot/MyMovieSESpring/MyMovieSeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.MyMovieSESpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMovieSeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMovieSeSpringApplication.class, args);
+	}
+
+}
