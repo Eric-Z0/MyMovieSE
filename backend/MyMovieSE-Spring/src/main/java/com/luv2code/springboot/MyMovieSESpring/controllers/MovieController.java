70ac869cf1658a7187ce829f46b32dc8ac332821
Eric-Z0 <ericzhuang603@gmail.com>
2020-01-14 22:23:05 -0500

    implement the filter function by using JAVA 8 stream feature

--- backend/MyMovieSE-Spring/src/main/java/com/luv2code/springboot/MyMovieSESpring/controllers/MovieController.java
+++ backend/MyMovieSE-Spring/src/main/java/com/luv2code/springboot/MyMovieSESpring/controllers/MovieController.java
@@ -1,6 +1,9 @@
 package com.luv2code.springboot.MyMovieSESpring.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -30,7 +33,7 @@ public class MovieController {
 	@Autowired
 	MovieRepository movieRepository;
 	
-	String[] best50Movies = {
+	String[] best50MoviesP1 = {
 		"Black Panther", "Avengers: Endgame", "Us", "Toy Story 4", "Mission: Impossible - Fallout",
 		"Lady Bird", "The Wizard of Oz", "The Irishman", "Citizen Kane", "BlacKkKlansman", "The Cabinet of Dr. Caligari",
 		"Get Out", "Spider-Man: Into the Spider-Verse", "Mad Max: Fury Road", "Modern Times", "Booksmart",
@@ -42,29 +45,78 @@ public class MovieController {
 		"Paddington 2", "The Favourite", "King Kong", "The Adventures of Robin Hood"		
 	};
 	
+	String[] best50MoviesP2 = {
+			"Metropolis", "Once Upon a Time In Hollywood", "Laura", "Gravity", "Top Hat", "Leave No Trace",
+			"Boyhood", "The Maltese Falcon", "Manchester by the Sea", "Argo", "12 Years a Slave", "Baby Driver",
+			"Zootopia", "War for the Planet of the Apes", "Psycho", "The Florida Project", "La La Land", "The Night of the Hunter",
+			"Spider-Man: Homecoming", "Widows", "Alien", "Seven Samurai", "North by Northwest", "Shoplifters",
+			"Sunset Boulevard", "Shazam", "Won't You Be My Neighbor?", "Hell or High Water", "Toy Story 2",
+			"Up", "Toy Story 3", "There will be blood", "Mulholland Drive", "The Dark Knight", "Tokyo Story",
+			"Lost in Translation", "Spirited Away", "Blade Runner", "Under the Skin", "Brokeback Mountain", "Boyhood"
+	};
+	
+	/*
+	// THE FOLLOWING COMMENTED OUT CODE ONLY USED FOR DB INITIALIZATION AND TESTING
 	
 	@GetMapping("/init")
 	public String initMovieDB() {
 		
-		final String url = "http://www.omdbapi.com/?s=joker&page=1&apikey=a9b731fa";
 		RestTemplate restTemplate = new RestTemplate();
-		String result = restTemplate.getForObject(url, String.class);
 		ObjectMapper mapper = new ObjectMapper();
 		MovieSnapshotResponse msRes;
-		try {
-			msRes = mapper.readValue(result, MovieSnapshotResponse.class);
-			MovieSnapshot[] msArray = msRes.getSearch();
-			for(int i=0; i<msArray.length; i++) {
-				movieSnapshotRepository.save(msArray[i]);
+		
+		for(int index=0; index < best50MoviesP2.length; index++) 
+		{
+			String movieUrl = String.format("http://www.omdbapi.com/?s=%s&apikey=a9b731fa", best50MoviesP2[index]);
+			String result = restTemplate.getForObject(movieUrl, String.class);
+			System.out.println();
+			
+			try {
+				msRes = mapper.readValue(result, MovieSnapshotResponse.class);
+				MovieSnapshot[] msArray = msRes.getSearch();
+				
+				movieSnapshotRepository.save(msArray[0]);
+				//for(int i=0; i<msArray.length; i++) {
+				//	movieSnapshotRepository.save(msArray[i]);
+				//}
+				
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
 			}
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
 		}
 		
 		return "Database has been initialized";
 	}
+	
+	
+	// An auxiliary function to load
+	@GetMapping("/addAllToMovieDB")
+	public String loadMovieDetailsToDB()
+	{
+		List<MovieSnapshot> msList = movieSnapshotRepository.findAll();
+		RestTemplate restTemplate = new RestTemplate();
+		ObjectMapper mapper = new ObjectMapper();
+		
+		for(int index=0; index < msList.size(); index++) {
+			String imdbID = msList.get(index).getImdbID();
+			String movieUrl = String.format("http://www.omdbapi.com/?i=%s&apikey=a9b731fa", imdbID);
+			String result = restTemplate.getForObject(movieUrl, String.class);
+			try {
+				Movie myMovie = mapper.readValue(result, Movie.class);
+				myMovie.setWriter("empty"); // a temporary fix to insert movie data into SQL
+				movieRepository.save(myMovie);
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return "Load movie details finished";
+	}
+	
 	
 	// An auxiliary function to load
 	@GetMapping("/addToMovieDB/i={id}")
@@ -84,6 +136,8 @@ public class MovieController {
 			e.printStackTrace();
 		}
 	}
+	
+	*/
 	
 	// When the front end starts this url will be requested
 	@GetMapping("/upload")
@@ -114,6 +168,7 @@ public class MovieController {
 	}
 	
 	// This function returns a detailed info related to a movie based on its id
+	// Note: this function may be not be used
 	@GetMapping("/movie/i={id}")
 	public String getMovieInfo(@PathVariable("id") String imdbId)
 	{
@@ -141,45 +196,75 @@ public class MovieController {
 			@PathVariable("country") String country,
 			@PathVariable("lang") String lang) 
 	{
+		List<Movie> movies = movieRepository.findAll();
 		
 		if(category != null && !category.isEmpty()) {
 			
-			System.out.println("Cat is selected!");
+			Stream<Movie> streamMovies =  movies.stream().filter(m -> m.getType().equals(category));
+			movies = streamMovies.collect(Collectors.toList());
+			//System.out.println("Cat is selected!");
 		}
 		
 		if(genre != null && !genre.isEmpty()) {
 			
-			System.out.println("genre is selected!");
+			Stream<Movie> streamMovies =  movies.stream().filter(m -> m.getGenre().equals(genre));
+			movies = streamMovies.collect(Collectors.toList());
+			//System.out.println("genre is selected!");
 		}
 		
 		if(year != null && !year.isEmpty()) {
 			
-			System.out.println("year is selected!");
+			Stream<Movie> streamMovies =  movies.stream().filter(m -> m.getYear().equals(year));
+			movies = streamMovies.collect(Collectors.toList());
+			//System.out.println("year is selected!");
 		}
 		
 		if(country != null && !country.isEmpty()) {
 			
-			System.out.println("country is selected!");
+			Stream<Movie> streamMovies =  movies.stream().filter(m -> m.getCountry().equals(country));
+			movies = streamMovies.collect(Collectors.toList());
+			//System.out.println("country is selected!");
 		}
 		
 		if(lang != null && !lang.isEmpty()) {
 			
-			System.out.println("lang is selected!");
+			Stream<Movie> streamMovies =  movies.stream().filter(m -> m.getLanguage().equals(lang));
+			movies = streamMovies.collect(Collectors.toList());
+			//System.out.println("lang is selected!");
 		}
 		
-		/*
-		// return movies based on filter type selected
-		// this is achieved by parsing url
-		ObjectMapper mapper = new ObjectMapper();
-		Movie myMovieFetched = movieRepository.findAll().get(0);
-		
-		try {
-			String jsonStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(myMovieFetched);
-			return jsonStr;
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
+		if(movies.size() != 0) {
+			
+			List<MovieSnapshot> msList = new ArrayList<>();
+			
+			for(int index=0; index < movies.size(); index++) {
+				String imdbID = movies.get(index).getImdbID();
+				MovieSnapshot ms = movieSnapshotRepository.findByImdbID(imdbID);
+				msList.add(ms);
+			}
+			
+			// Convert List to Array
+			MovieSnapshot[] msArray = new MovieSnapshot[msList.size()];
+			
+			for(int index=0; index < msList.size(); index++) {
+				msArray[index] = msList.get(index);
+			}
+			
+			MovieSnapshotResponse msRes = new MovieSnapshotResponse();
+			msRes.setSearch(msArray);
+			msRes.setTotalResults("" + msArray.length);
+			msRes.setResponse("True");
+			
+			ObjectMapper mapper = new ObjectMapper();
+			
+			try {
+				String jsonStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(msRes);
+				return jsonStr;
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+			
 		}
-		*/
 		
 		return "No matching movie found";
 	}
--- backend/MyMovieSE-Spring/src/main/java/com/luv2code/springboot/MyMovieSESpring/repository/MovieSnapshotRepository.java
+++ backend/MyMovieSE-Spring/src/main/java/com/luv2code/springboot/MyMovieSESpring/repository/MovieSnapshotRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.luv2code.springboot.MyMovieSESpring.models.MovieSnapshot;
 
 public interface MovieSnapshotRepository extends JpaRepository<MovieSnapshot, Long>{
-
+	MovieSnapshot findByImdbID(String imdbID);
 }
